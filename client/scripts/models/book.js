f293f89157fc5e401176ecb2686256b047fb4f62
Jeff Martinez <jeffxmartinez@gmail.com>
2017-11-08 10:48:15 -0800

    update

--- client/scripts/models/book.js
+++ client/scripts/models/book.js
@@ -2,29 +2,26 @@
 var app = app || {};
 var __API_URL__ = 'https://wr-jm-booklist.herokuapp.com/';
 
-(function(module) {
-  function errorCallback(err) {
+(function (module) {
+  function errorCallback (err) {
     module.errorView.initErrorPage(err);
   }
 
-function Book(bookObject) {
-  Object.keys(bookObject).forEach(key => this[key] = taskObject[key]);
-}
+  function Book (bookObject) {
+    Object.keys(bookObject).forEach(key => this[key] = taskObject[key]);
+  }
 
-Book.prototype.toHtml = function() {
-  let template = Handlebars.compile($('.book-template').text());
-  return template(this);
-}
+  Book.prototype.toHtml = function () {
+    let template = Handlebars.compile($('#book-template').text());
+    return template(this);
+  };
 
-Book.all = [];
+  Book.all = [];
+  Book.loadAll = rows => {
+    Book.all = rows.map(book => new Book(book));
+  };
+  Book.fetchAll = callback =>
+    $.get(`${__API_URL__}books`).then(Book.loadAll).then(callback).catch(errorCallback);
 
-Book.loadAll = rows => {
-  Book.all = rows.map(book => new Book(book));
-}
-
-Book.fetchAll = callback =>
-  $.get(`${__API_URL__}books`).then(Book.loadAll).then(callback).catch(errorCallback);
-
-module.Book = Book;
-
+  module.Book = Book;
 })(app);
