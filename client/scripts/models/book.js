76b7bc6dc8e6b638735401cccdd6602906fa0c36
Jeff Martinez <jeffxmartinez@gmail.com>
2017-11-08 11:21:11 -0800

    update

--- client/scripts/models/book.js
+++ client/scripts/models/book.js
@@ -7,24 +7,23 @@ var __API_URL__ = 'https://wr-jm-booklist.herokuapp.com/';
     module.errorView.initErrorPage(err);
   }
 
-function Book(bookObject) {
-  Object.keys(bookObject).forEach(key => this[key] = taskObject[key]);
-}
+  function Book(bookObject) {
+    Object.keys(bookObject).forEach(key => this[key] = taskObject[key]);
+  }
 
-Book.prototype.toHtml = function() {
-  let template = Handlebars.compile($('.book-template').text());
-  return template(this);
-}
+  Book.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#book-list-template').text());
+    return template(this);
+  };
 
-Book.all = [];
+  Book.all = [];
 
-Book.loadAll = rows => {
-  Book.all = rows.map(book => new Book(book));
-}
+  Book.loadAll = rows => {
+    Book.all = rows.map(book => new Book(book));
+  };
 
-Book.fetchAll = callback =>
-  $.get(`${__API_URL__}books`).then(Book.loadAll).then(callback).catch(errorCallback);
+  Book.fetchAll = callback =>
+    $.get(`${__API_URL__}/api/v1/books`).then(Book.loadAll).then(callback).catch(errorCallback);
 
-module.Book = Book;
-
+  module.Book = Book;
 })(app);
